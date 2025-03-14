70bed4dc07c6879187df7d84324d1ed5f8aef042
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-03-14 15:46:16 +0600

    fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [{
+            source: '/',
+            destination: '/home',
+            permanent: true,
+        },];
+    },
+};
 
 export default nextConfig;
