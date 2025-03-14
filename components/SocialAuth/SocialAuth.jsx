34e021309e322b00d8be4594b2b1f4a7d2833dfb
istiak19 <istiak15-14128@diu.edu.bd>
2025-03-14 15:13:00 +0600

    added google& github nextAuth.js

--- components/SocialAuth/SocialAuth.jsx
+++ components/SocialAuth/SocialAuth.jsx
@@ -2,10 +2,24 @@
 
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const SocialAuth = () => {
+    const router = useRouter();
+    const session = useSession();
     const handleSocialAuth = async (providerName) => {
         console.log(providerName);
+        await signIn(providerName);
     }
+
+    console.log(session)
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/')
+        }
+    }, [session?.status])
 
     return (
         <div className="flex space-x-6 justify-center">
@@ -13,14 +27,14 @@ const SocialAuth = () => {
             <button
                 type="button"
                 onClick={() => handleSocialAuth("google")}
-                className="w-14 h-14 flex items-center justify-center rounded-full bg-gray-100 border border-blue-300 shadow-md hover:shadow-lg transition">
+                className="w-14 btn h-14 flex items-center justify-center rounded-full bg-gray-100 border border-blue-300 shadow-md hover:shadow-lg transition">
                 <FcGoogle className="text-2xl" />
             </button>
             {/* GitHub */}
             <button
                 type="button"
                 onClick={() => handleSocialAuth("github")}
-                className="w-14 h-14 flex items-center justify-center rounded-full bg-gray-100 border border-blue-300 shadow-md hover:shadow-lg transition">
+                className="w-14 btn h-14 flex items-center justify-center rounded-full bg-gray-100 border border-blue-300 shadow-md hover:shadow-lg transition">
                 <FaGithub className="text-black text-2xl" />
             </button>
         </div>
