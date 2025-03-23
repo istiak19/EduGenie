428cdae63a7f24878ad9beec3a41dbad99811ba5
istiak19 <istiak15-14128@diu.edu.bd>
2025-03-23 14:17:17 +0600

    get by course info

--- app/api/course/route.js
+++ app/api/course/route.js
@@ -1,6 +1,10 @@
 import dbConnect, { collectionNames } from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
 
+export async function GET(req) {
+    const result = await dbConnect(collectionNames.courseCollection).find().toArray();
+    return NextResponse.json(result);
+};
 export async function POST(req) {
     const courseInfo = await req.json();
     const result = await dbConnect(collectionNames.courseCollection).insertOne(courseInfo);
