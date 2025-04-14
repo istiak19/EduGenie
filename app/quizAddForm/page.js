7a04405514c9af3856a8d1417af13456c3c96bd2
showrov4g <showrovg00@gmail.com>
2025-04-14 23:19:18 +0600

    quize add from make successfully

---
+++ app/quizAddForm/page.js
@@ -0,0 +1,12 @@
+import QuizAddFrom from '@/components/QuizAddFrom/QuizAddFrom'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <QuizAddFrom/>
+    </div>
+  )
+}
+
+export default page
