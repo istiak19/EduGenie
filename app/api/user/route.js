e754ede8d822b5b0871e4167607dab7ab240f81f
showrov4g <showrovg00@gmail.com>
2025-04-15 12:25:45 +0600

    clear every thing for building form new

--- app/api/user/route.js
+++
@@ -1,8 +0,0 @@
-import dbConnect, { collectionNames } from "@/lib/dbConnect";
-import { NextResponse } from "next/server";
-
-// Get by course info
-export async function GET(req) {
-    const result = await dbConnect(collectionNames.userCollection).find().toArray();
-    return NextResponse.json(result);
-};
